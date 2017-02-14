e551e82460169a3ce542f8c933d67ebcc5ab48da
Eric <eric11mk@gmail.com>
2017-02-13 22:13:57 -0500

    adding broken post process code

--- IlluminationComparison/PostProcesser.h
+++ IlluminationComparison/PostProcesser.h
@@ -1,9 +1,24 @@
 #pragma once
+#include <DirectXMath.h>
+#include "DXMathImpl.h"
+#include "SimpleShader.h"
+
 class PostProcesser
 {
 public:
 	PostProcesser();
 	~PostProcesser();
+
+private:
+	ID3D11RenderTargetView* postProcessRTV;
+	ID3D11RenderTargetView* ssaoRTV;
+	ID3D11RenderTargetView* bloomExtractRTV; // will also be used for blurring
+	ID3D11RenderTargetView* bloomHorizonatalRTV;
+
+	ID3D11ShaderResourceView* postProcessSRV;
+	ID3D11ShaderResourceView* ssaoSRV;
+	ID3D11ShaderResourceView* bloomExtractSRV; // will also be used for blurring
+	ID3D11ShaderResourceView* bloomHorizonatalSRV;
 };
 
 /*
